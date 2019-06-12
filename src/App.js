50ac8690ea772f7cb3eaef0d2a146ec4ce826337
Will Ting <theweicode@gmail.com>
2019-06-12 15:44:15 -0700

    starting the layout of the new portfolio site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+     <div className="header">
+       <div className="mobile-home">
+       Hello, I'm <text className="name">William Ting</text>. I'm a fullstack web developer.
+       <div className="btn">View my work <i className="fas fa-arrow-right"></i></div>
+       </div>
+     
+     </div>
+     <div className="projects">PROJECTS</div>
+     <div className="real-estate">REAL ESTATE APP</div>
     </div>
   );
 }
