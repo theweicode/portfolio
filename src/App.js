771349a8f23bd0efa6a878538bc8913e256a3ab9
theweicode <theweicode@gmail.com>
2019-06-15 11:43:25 -0700

    adding canvas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, {Component} from 'react';
-import './App.scss';
-import * as Scroll from 'react-scroll';
-import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
- 
+import React, { Component } from "react";
+import "./App.scss";
+import * as Scroll from "react-scroll";
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from "react-scroll";
+
+import Canvas from "./component/Canvas";
 
 class App extends Component {
   constructor() {
@@ -11,41 +19,45 @@ class App extends Component {
       name: "Will",
       isActive: false,
       position: 0
-    
     };
     this.scrollTo = this.scrollTo.bind(this);
   }
+
   scrollTo() {
-    scroll.scrollTo(667);
+    scroll.scrollTo(1640);
   }
 
   render() {
     return (
-      <div className="wrapper">
-       <div className="header">
-         <div className="mobile-home">
-         Hello, I'm <text className="name">William Ting</text>. <br></br> I'm a fullstack web developer.
-         <div className="btn" onClick={this.scrollTo}>View my work <i className="fas fa-arrow-right"></i></div>
-         </div>
-       
-       </div>
-       <div className="abt-sct">
-         <div className="title">ABOUT <br></br> <div className="line">_____</div>
-         
-         <div className="box-1">box1</div>
-         <div className="box-2">box2</div>
-         <div className="box-3">box3</div>
-         <div className="box-4">box4</div>
-         <div className="box-5">box5</div>
-         
-         </div>
-       </div>
-       <div className="projects">Project-Section<div className="real-estate">REAL ESTATE APP</div></div>
-       
-      </div>
+      <>
+        <div className="wrapper">
+          <div className="header">
+            <div className="mobile-home">
+              Hello, I'm <text className="name">William Ting</text>. <br /> I'm
+              a fullstack web developer.
+              <div className="btn" onClick={this.scrollTo}>
+                View my work <i className="fas fa-arrow-right" />
+              </div>
+            </div>
+          </div>
+          <Canvas />
+          <div className="abt-sct">
+            <div className="title">
+              ABOUT <br /> <div className="line">_____</div>
+              <div className="box-1">box1</div>
+              <div className="box-2">box2</div>
+              <div className="box-3">box3</div>
+              <div className="box-4">box4</div>
+              <div className="box-5">box5</div>
+            </div>
+          </div>
+          <div className="projects">
+            Project-Section<div className="real-estate">REAL ESTATE APP</div>
+          </div>
+        </div>
+      </>
     );
   }
- 
 }
 
 export default App;
